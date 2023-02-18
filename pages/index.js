a776ef2a4daa7491241f81f11bcc051e55b76238
RonnyLV <hamste126@gmail.com>
2023-02-18 19:26:19 +0200

    Initial commit

--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,35 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className='container'>
       <Head>
-        <title>Next.js Starter!</title>
+        <title>De-mine UA</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
+        <Header title="Low-cost metal detection solution for UA's mined land" />
+        <p className="col description">
+          A metal detector but for a of the price.
         </p>
+        <div class="col col-12 sketchfab-embed-wrapper">
+          <iframe title="Walkman WM-F35 SONY" frameborder="0" allowfullscreen mozallowfullscreen="true"
+            webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking"
+            style={{width:"calc(50vw)", height:"calc(50vh)" }}
+            xr-spatial-tracking execution-while-out-of-viewport
+            execution-while-not-rendered web-share src="https://sketchfab.com/models/5d8f30c391874f20a22f070720ef3c3c/embed"> </iframe>
+        
+        </div>
+        <div className="col">
+          <Header title="Why?"></Header>
+          <p className="col description">
+            A significant portion of the land in Ukraine is contaminated with landmines, which will require clearance.
+          </p>
+        </div>
+        <div className="col">
+          <img src="landmines_area.png" style={{ width: "calc(60vh)" }} />
+        </div>
       </main>
-
-      <Footer />
     </div>
   )
 }
